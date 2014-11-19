61757add2b2ead34701834c809151bf3f72d8f6b
Damian Myerscough <Damian.Myerscough@gmail.com>
2014-11-18 23:11:01 -0800

    Stop Salt from running the checks until Sensu has perform an initial check

--- meditation.py
+++ meditation.py
@@ -2,25 +2,36 @@
 
 import requests
 import logging
+import smtplib
 import celery
+import redis
 
 import salt.client
 
 log = logging.getLogger(__name__)
 
 
-class SaltRemedy(object):
-    def __init__(self, hostname, username, password, location=None):
+class Meditation(object):
+    def __init__(self,
+                 hostname,
+                 username,
+                 password,
+                 redis,
+                 redis_port=6379,
+                 location=None):
+
         self.hostname = hostname
         self.username = username
         self.password = password
 
+        self.redis = redis
+        self.redis_port = redis_port
+
         self.location = location
-        self.acked = {}
 
     def __get_events(self):
         '''
-
+        Grab Sensu Events
         '''
         req = requests.get(self.hostname + '/events',
                            auth=(self.username, self.password))
@@ -35,48 +46,56 @@ class SaltRemedy(object):
 
     def proc(self):
         '''
+        Parse the current Sensu events and have Salt run remediation actions
+        against problematic hosts
 
         TODO: Validate remedy location
+              Read from configuration to set email address etc
         '''
+        _jobs = redis.StrictRedis(host=self.redis, port=self.redis_port)
         client = salt.client.LocalClient()
+
+        status = {'success': {}, 'fail': {}}
 
         for event in self.__get_events():
             eventId = '|-'.join([event['client']['name'],
                                  event['check']['name']])
 
-            if self.acked.get(eventId, False):
-                self.acked[eventId] = event
-            else:
+            # Dont even run the check if we have already run remediation
+            if not _jobs.get(eventId):
                 if self.location:
-                    remedy = '{0}/{1}'.format(self.location,
-                                              event['check']['name'])
-
-                    res = client.cmd(event['client']['name'],
-                                     'state.sls',
-                                     [remedy])
+                    state = '/'.join([self.location, event['check']['name']])
                 else:
-                    res = client.cmd(event['client']['name'],
-                                     'state.sls',
-                                     [event['check']['name']])
+                    state = event['check']['name']
 
-                if isinstance(res[event['client']['name']], list):
-                    raise Exception('Unable to execute remedy: {0}'.format(
-                        res[event['client']['name']][0]))
-                else:
+                res = client.cmd(event['client']['name'], 'state.sls', [state])
+
+                if isinstance(res[event['client']['name']], dict):
                     for i in res[event['client']['name']].items():
                         action = i[-1]
 
-                        # TODO: Email operator when a remedy fails
                         if action['result']:
-                            print "Remedy, run"
+                            status['success'].update({action['name']:
+                                                      action['comment']})
                         else:
-                            print action['name'], " failed to execute: `{0}`".format(action['comment'])
+                            status['fail'].update({action['name']:
+                                                   action['comment']})
 
-                # Acknowledge that we tried fixing the issue
-                self.acked[eventId] = event
+                    # Acknowledge that we tried to fix the issue. We should not
+                    # attempt to run the state again until sensu has rechecked
+                    # the service again
+                    _jobs.set(eventId, True)
+                    _jobs.expire(eventId, (event['check']['interval'] * 2))
+
+                    msg = 'Subject: Meditation {0} was executed\n \
+                           The following states were successfully run: X\n\n \
+                           The following states failed to run: X\n\n'.format(event['check']['name'])
+
+                    mail = smtplib.SMTP('localhost')
+                    mail.sendmail('damian@mirulabs.com', 'Damian.Myerscough@gmail.com', msg)
 
 
 if __name__ == '__main__':
 
-    sensu = SaltRemedy('http://localhost:4567', 'admin', 'mypass', 'remedy')
+    sensu = Meditation('http://localhost:4567', 'admin', 'mypass', 'localhost', location='remedy')
     sensu.proc()
